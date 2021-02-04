3583add7d15aafe699d9c8a0a51cfecaf44b5ccd
Kevin <kbrowne97@gmail.com>
2021-02-03 20:06:54 -0500

    adds js modules

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("synced"),console.log("????");
+(()=>{"use strict";document.querySelector("#content"),document.querySelector(".new-project").addEventListener("click",(()=>{const e=document.querySelector("#content");console.log("running");const t=document.createElement("div");t.classList.add(".new-project-modal"),e.appendChild(t)}))})();
